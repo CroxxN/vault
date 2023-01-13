da7281a2a84ab66ddd9f1847a656a1ff8e65c900
CroxxN <saradgaihre19@gmail.com>
2023-01-13 22:25:58 +0545

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# A Encryption and hashing library
+
+## Vault is a Encryption and hashing library written in Rust
+
+### Implementation goals:
+
+- Secure Hash Algorithm Family:
+  - [ ] SHA0 
+  - [ ] SHA1 
+  - [ ] SHA2 
+
+- AES Family:
+  - [ ] AES-128
+  - [ ] AES-192
+  - [ ] AES-256
+
+- CRC:
+  - [ ] CRC8
+  - [ ] CRC16
+  - [ ] CRC32
+
+  
---
+++ sha1/src/lib.rs
@@ -0,0 +1,74 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+use std::path;
+#[derive(Debug, PartialEq)]
+enum CalcMode {
+    Performance,
+    Storage,
+}
+
+pub struct Sha1Builder {
+    calc_mode: CalcMode,
+}
+
+pub struct Sha1 {
+    mode: CalcMode,
+}
+
+impl Default for Sha1Builder {
+    fn default() -> Self {
+        Sha1Builder {
+            calc_mode: (CalcMode::Performance),
+        }
+    }
+}
+
+impl Sha1Builder {
+    pub fn performance(self) -> Self {
+        Self {
+            calc_mode: CalcMode::Performance,
+        }
+    }
+    pub fn storage(self) -> Self {
+        Self {
+            calc_mode: CalcMode::Storage,
+        }
+    }
+    pub fn build(self) -> Sha1 {
+        Sha1 {
+            mode: self.calc_mode,
+        }
+    }
+}
+
+impl Sha1 {
+    pub fn new() -> Sha1 {
+        Sha1 {
+            mode: CalcMode::Performance,
+        }
+    }
+    pub fn new_opts() -> Sha1Builder {
+        Sha1Builder::default()
+    }
+    fn new_from_file(&self) {
+        unimplemented!()
+    }
+    fn from_bytes(_: &[u8]) {
+        unimplemented!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new() {
+        let sha1 = Sha1::new();
+        assert_eq!(sha1.mode, CalcMode::Performance);
+    }
+    fn new_with_opts() {
+        let sha1 = Sha1::new_opts().storage().build();
+        assert_eq!(sha1.mode, CalcMode::Storage);
+    }
+}
