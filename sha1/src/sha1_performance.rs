af526910222f420532530f9956a8696145918dbd
CroxxN <saradgaihre19@gmail.com>
2023-01-20 22:56:45 +0545

    working hash <512 bits

--- sha1/src/sha1_performance.rs
+++ sha1/src/sha1_performance.rs
@@ -1,12 +1,8 @@
-#![allow(unused_variables)]
-
 // Will also be used in the storage-efficient implementation
 
 // TODO: Remove unsafe, implement message to word array, and make the code ergonomic
 
 use crate::constants::K;
-use std::io::Read;
-use std::path::PathBuf;
 
 macro_rules! shift_rotate {
     ($num:literal ,$expression:expr) => {
@@ -40,18 +36,44 @@ impl Sha1 {
     pub fn get_words(&self, n: usize) -> u32 {
         self.word[n]
     }
-    pub fn process_hash(&mut self, input: &[u8]) {
+    pub fn append_hash(&mut self, input: &[u8]) {
+        if input.len() <= 56 {
+            self.process_hash_once(input);
+            return;
+        }
+        self.process_hash_once(&input[..=56]);
+        self.num_blocks = (input.len() / 57) as u64;
+        let mut curr_chunk: usize = 57;
+        for i in 0..(self.num_blocks - 2) {
+            curr_chunk = curr_chunk + 57;
+            println!("Printing Loop {i}");
+            self.process_hash_multiple(&input[(curr_chunk - 57)..curr_chunk]);
+        }
+        self.process_hash_multiple(&input[curr_chunk..]);
+    }
+    pub fn reset_hash(self) -> Self {
+        Sha1::new()
+    }
+    fn initialize_bits(input: &[u8]) -> [u8; 64] {
         let temp: &mut [u8; 64] = &mut [0; 64];
-        let length = input.len() as u64;
-        self.message_length = length * 8;
         temp.iter_mut().zip(input).for_each(|(t, i)| {
             *t = *i;
         });
+        temp.to_owned()
+    }
+    fn process_hash_once(&mut self, input: &[u8]) {
+        let mut temp = Self::initialize_bits(input);
+        self.message_length = (8 * input.len()) as u64;
         temp[input.len()] = temp[input.len()] | 0x80;
+        self.hash(&temp);
+    }
+    fn process_hash_multiple(&mut self, input: &[u8]) {
+        let temp = Self::initialize_bits(input);
+        self.message_length = (8 * input.len()) as u64;
+        self.hash(&temp);
         // for i in 56..64 {
         //     temp[i] = ((length * 8) << ((i - 56) * 8)) as u8;
         // }
-        self.hash(temp);
     }
     fn hash(&mut self, blocks: &[u8; 64]) {
         self.word
@@ -82,7 +104,7 @@ impl Sha1 {
     fn f(&self, i: &usize) -> u32 {
         if *i < 20 {
             (self.f_buf[1] & self.f_buf[2]) | (!self.f_buf[1] & self.f_buf[3])
-        } else if *i >= 40 || *i <= 59 {
+        } else if *i >= 40 && *i <= 59 {
             (self.f_buf[1] & self.f_buf[2])
                 | (self.f_buf[1] & self.f_buf[3])
                 | (self.f_buf[2] & self.f_buf[3])
@@ -131,34 +153,43 @@ mod test {
     #[test]
     fn hash() {
         let mut sha = Sha1::new();
-        sha.process_hash(b"abc");
+        sha.append_hash(b"abc");
         assert_eq!(
             sha.get_hash(),
             [0xA9993E36, 0x4706816A, 0xBA3E2571, 0x7850C26C, 0x9CD0D89D]
         );
     }
     #[test]
+    fn long_hash() {
+        let mut sha = Sha1::new();
+        sha.append_hash(b"abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq");
+        assert_eq!(
+            sha.get_hash(),
+            [0x84983E44, 0x1C3BD26E, 0xBAAE4AA1, 0xF95129E5, 0xE54670F1]
+        );
+    }
+    #[test]
     fn first_word() {
         let mut sha = Sha1::new();
-        sha.process_hash(b"abc");
+        sha.append_hash(b"abc");
         assert_eq!(sha.get_words(0), 0x61626380);
     }
     #[test]
     fn second_word() {
         let mut sha = Sha1::new();
-        sha.process_hash(b"abc");
+        sha.append_hash(b"abc");
         assert_eq!(sha.get_words(1), 0x0);
     }
     #[test]
     fn last_word() {
         let mut sha = Sha1::new();
-        sha.process_hash(b"abc");
+        sha.append_hash(b"abc");
         assert_eq!(sha.get_words(15), 0x18);
     }
     #[test]
     fn seven_word() {
         let mut sha = Sha1::new();
-        sha.process_hash(b"abc");
+        sha.append_hash(b"abc");
         assert_eq!(sha.get_words(7), 0x0);
     }
 }
